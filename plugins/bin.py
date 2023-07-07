7713c64d8b9bbd51fa2d3a6943493db568a58326
Bai <bai@dotogn.com>
2023-07-07 21:19:11 +0800

    修复查找bin的api

--- plugins/bin.py
+++ plugins/bin.py
@@ -19,29 +19,30 @@ async def card(client: Client, msg: Message):
     me=await client.get_me()
     # print(me,dir(me),me.id,me.username)
     try:
-        resp=await session.get("https://lookup.binlist.net/{}".format(arg),timeout=5.5)
-        if resp.status == 404:
+        resp=await session.get("https://api.freebinchecker.com/bin/{}".format(arg),timeout=5.5)
+        
+        # if resp.status == 404:
+        #     await msg.edit_text("卡头不存在")
+        #     return 
+        # elif resp.status==429:
+        #     await msg.edit_text("请求速度过快,请稍后再试")
+        #     return 
+        # elif  resp.status !=200:
+        #     await msg.edit_text("发生其他错误")
+        #     return 
+        bin_json=await resp.json()
+        if bin_json['valid'] == "false":
             await msg.edit_text("卡头不存在")
             return 
-        elif resp.status==429:
-            await msg.edit_text("请求速度过快,请稍后再试")
-            return 
-        elif  resp.status !=200:
-            await msg.edit_text("发生其他错误")
-            return 
-        bin_json=await resp.json()
-        if "name" in bin_json['bank']:
-            bank_name=bin_json['bank']["name"]
+        if "name" in bin_json['issuer']:
+            bank_name=bin_json['issuer']["name"]
         else:
             bank_name="未知"
-        txt=f"[*]卡BIN: {arg}\n[*]卡品牌: {bin_json['scheme']}\n[*]卡类型: {bin_json['type']}\n[*]卡级别: {bin_json['brand']}\n[*]发卡行: {bank_name}\n[*]发卡国家: {bin_json['country']['name']}\n"
-        if "prepaid" in bin_json:
-            if bin_json["prepaid"]:
-                txt+="[*]是否为预付卡:是\n"
-            else:
-                 txt+="[*]是否为预付卡:否\n"   
+        txt=f"[*]卡BIN: {arg}\n[*]卡品牌: {bin_json['card']['scheme']}\n[*]卡类型: {bin_json['card']['type']}\n[*]发卡行: {bank_name}\n[*]发卡国家: {bin_json['country']['name']}\n"
+        if bin_json["card"]["prepaid"]=="true":
+            txt+="[*]是否为预付卡:是\n"
         else:
-            txt+="[*]是否为预付卡:未知\n"
+            txt+="[*]是否为预付卡:否\n"   
         await msg.edit_text(txt)
     except Exception as e:
         await msg.edit_text(e)
