8a1b43824da3efdeb5bcd1ac555790435206e3e5
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2023-10-22 21:03:17 +0800

    移植pagermaid base64 插件

---
+++ plugins/utils_base64.py
@@ -0,0 +1,37 @@
+from base64 import b64decode, b64encode
+
+from pyrogram import Client
+from pyrogram.types import Message
+
+from core import command
+from tools.helpers import Parameters
+
+"""
+data/command.yml
+
+base64:
+  cmd: base64
+  format: base64 <e|d> text
+  usage: 使用base64 编解码文字内容
+"""
+
+
+@Client.on_message(command("base64"))
+async def base64(_: Client, message: Message):
+    cmd, args = Parameters.get(message)
+    msg = args
+    if not msg:
+        return await message.edit_text("`出错了呜呜呜 ~ 无效的参数。`")
+    args_list = args.split(" ")
+    subcmd = args_list[0].trim()
+    msg = args_list[1].trim()
+    if subcmd == "e":
+        if result := b64encode(msg.encode("utf-8")).decode("utf-8"):
+            await message.edit_text(f"`{result}`")
+    if subcmd == "d":
+        try:
+            result = b64decode(msg).decode("utf-8")
+        except Exception as e:
+            return await message.edit_text("`出错了呜呜呜 ~ 无效的参数。`")
+        if result:
+            await message.edit_text(f"`{result}`")
