381b7df60cf715139be04f38be70d330d0e47753
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2023-10-24 22:53:14 +0800

    添加插件-历史上的今天

--- plugins/services/utils_history_today.py
+++ plugins/services/utils_history_today.py
@@ -1,15 +1,27 @@
 from pyrogram import Client
+from pyrogram.enums import ParseMode
 from pyrogram.types import Message
 
 from core import command
 from tools.sessions import session
 
 
-async def get_history_today_info() -> str:
+async def get_history_today_info(api_url: str) -> str:
     """
     获取历史上的今天，并格式化输出
     """
-    pass
+    async with session.get(api_url) as response:
+        if response.status == 200:
+            data = await response.json()
+            data_list = data['result'][:-1]
+            google_search = f"https://www.google.com/search?q="
+            markdown_list = [f"[{item['year']}年 {item['title']}]({google_search + item['title']})" for item in
+                             data_list]
+            md = "\n".join(markdown_list)
+            result = f"📅 | **历史上的今天** | `{data['today']}`\n{md}"
+            return result
+        else:
+            raise Exception(f"HTTP request failed with status code {response.status}")
 
 
 """
@@ -26,9 +38,12 @@ histoday:
 async def dictionary(_: Client, msg: Message):
     """历史上的今天"""
     api_url = "https://www.ipip5.com/today/api.php?type=json"
-
-    # with session.get()
-    #
-    # await msg.edit_text(f"正在准备{}。")
-    # TODO finish me
-    pass
+    try:
+        data = await get_history_today_info(api_url)
+        await msg.edit_text(
+            text=data,
+            parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True
+        )
+    except Exception as e:
+        await msg.edit_text(f"api获取数据失败: {e.message}")
