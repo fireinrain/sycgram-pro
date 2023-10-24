1634899edaf92f959a127852de4a302581ddaf38
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2023-10-25 02:27:37 +0800

    初步完成

--- plugins/functions/func_iptrace.py
+++ plugins/functions/func_iptrace.py
@@ -1,3 +1,4 @@
+# ip 回程路由测试
 from pyrogram import Client
 from pyrogram.types import Message
 
@@ -6,15 +7,16 @@ from core import command
 """
 data/command.yml
 
-dictionary:
-  cmd: dict
-  format: -dict <enzh|zhen> <单词或文字>
-  usage: 在线英汉-汉英字典服务
+
+iptrace:
+  cmd: iptrace
+  format: -iptrace <back/best>
+  usage: 主机回程路由测试(back 运行backtrace,best 运行besttrace)
 """
 
 
-@Client.on_message(command('dictionary'))
+@Client.on_message(command('iptrace'))
 async def dictionary(_: Client, msg: Message):
-    """英汉汉英字典查询"""
+    """主机回程线路测试"""
     await msg.edit_text(f"当前功能正在施工🚧中,请耐心等待插件上线")
     # TODO finish me
